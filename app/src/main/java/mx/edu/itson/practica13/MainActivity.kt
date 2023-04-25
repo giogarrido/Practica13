1b2b6542b1643e259b3389d58dbd0a3d8861dba6
Garrido <giovanni.garrido228724@potros.itson.edu.mx>
2023-04-25 00:46:32 -0700

    2do

--- app/src/main/java/mx/edu/itson/practica13/MainActivity.kt
+++ app/src/main/java/mx/edu/itson/practica13/MainActivity.kt
@@ -2,10 +2,24 @@ package mx.edu.itson.practica13
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import mx.edu.itson.practica13.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+
     }
 }
